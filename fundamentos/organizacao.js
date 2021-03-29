795f258c0671c04458b8c23e71677499fcc1d88a
Wesley Costa <wesferrcosta@gmail.com>
2021-03-28 22:49:10 -0300

    Organização  Básica de um código JS

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,11 @@
+console.log("Linha de codigo")
+
+// JS é organizado em sentenças de codigo e blocos
+// que podem ou não terminar em ponto e virgula
+
+{
+  {
+    console.log("Ola,")
+    console.log('Mundo!')
+  }
+}
