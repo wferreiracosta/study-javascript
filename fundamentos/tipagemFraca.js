d1b6d1c1209fafab481b918adaac0b1a2154939d
Wesley Costa <wesferrcosta@gmail.com>
2021-03-29 23:37:14 -0300

    Tipagem fraca

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,17 @@
+/*
+JS é fracamente tipada
+tipo da variavel é definido na atribuição
+*/
+
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer) // String
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer) // number
+
+// Evite nomes genericos e siglas para varivaies
+let valor = '' // Errado
+let numero = 1 // Errado
+let pqp = false // Errado
