d6c8bdf8a0ac2be558e9c446e29abfff5666cd8f
Wesley Costa <wesferrcosta@gmail.com>
2021-03-29 22:45:55 -0300

    O Básico de Var, Let e Const

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,27 @@
+var a = 3
+let b = 4
+
+var a = 30
+// let b = 40
+b = 40
+
+/*
+Variaveis declaradas com var podem ser redeclaradas com o mesmo nome e com var
+variavies declaradas com let so podem ser declaradas com o mesmo nome se estiverem em blocos de codigos diferentes
+*/
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 100
+// c = 500
+/*
+Constante so podem ter atribuição de valor uma unica vez
+seu valor é constante e não pode ser alterado
+*/
+
+console.log(c)
