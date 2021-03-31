b3b2f1410c6b19b9f8165013ce0426c585c5e012
Wesley Costa <wesferrcosta@gmail.com>
2021-03-30 22:36:51 -0300

    Number: Alguns cuidados

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,5 @@
+console.log(7 / 0) // Valor infinito
+console.log("10" / 2) // Verifica se a String pode ser convertida para valor numerico
+console.log("show" * 2) // JS não vai conseguir converter "show" para number então vai resultar em NaN -> Not a Number
+console.log(0.1 + 0.7) // Por causa da especificação de numeros pontos flutuantes o resultado não será 0.8
+//console.log(10.toString()) // Consegue em cima do Number (Number.toString()) mas não na escrita do numero
